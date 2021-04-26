7af91753d884c740635e9ccf25ac8c9c3cc6b2aa
manojthetechguy <manojthetechguy@gmail.com>
2021-04-26 16:49:05 +0530

    .

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,13 @@ import "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-  apiKey: "AIzaSyAndw4aPEMlYotuSDYf1zwOndPC5CsZxxM",
-  authDomain: "slack-clone-scribe.firebaseapp.com",
-  projectId: "slack-clone-scribe",
-  storageBucket: "slack-clone-scribe.appspot.com",
-  messagingSenderId: "31000604056",
-  appId: "1:31000604056:web:8dd1d56e804060e510cfed",
-  measurementId: "G-B7HETMS3KP",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 // Initialize Firebasez
 firebase.initializeApp(firebaseConfig);
